a03832ad7212a87987dc3c2a58acbe4f8f9d83c2
Gemuraiii <glenfjr95@gmail.com>
2021-05-22 20:06:46 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('There are obvious observations and conclusions you can draw from how you are treated. You are not something someone can pick up at any time when they are ready. Over my dead body'));
+app.get('/', (req, res) => res.send('There are obvious observations and conclusions you can draw from how you are treated. You are not something someone can pick up at any time when they are ready. Over my dead body. Make sure the conclusions are based off of facts or pure feelings and not anxiety at an all time high. Announce that respect is needed and see how you feel after.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
